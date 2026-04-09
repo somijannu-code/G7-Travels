ba72ef1095c865c9cbc97eb4e43eeafe64292ceb
somijannu-code <somijannu@gmail.com>
2026-04-09 10:39:14 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -348,7 +348,7 @@ export default function G7TravelsPage() {
                 window.scrollTo({ top: 0, behavior: 'smooth' })
               }}
             >
-              <Settings className="w-5 h-5 mr-2" />
+              <Settings className=border-2 border-white text-white hover:bg-white/10" />
               Advanced Booking
             </Button>
           </div>
